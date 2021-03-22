e6dee2a18853eee80b0ad51f478abae5868e2308
Bundit Hongmanee <bundit.j.h@gmail.com>
2021-03-21 23:08:40 -0700

    Create header component

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import styles from "../styles/header.module.scss";
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>Application Buddy</h1>
+    </header>
+  );
+}
+
+export default Header;
